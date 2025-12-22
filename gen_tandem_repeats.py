aac756e1ac2f2e7bab039dbba63f2a726672831e
i-xiaohu <631751599@qq.com>
2025-12-22 15:38:58 +0800

    gen_tandem_repeats.py: add non-repetitive flanks.

--- gen_tandem_repeats.py
+++ gen_tandem_repeats.py
@@ -2,9 +2,12 @@ import random
 import pandas as pd
 
 
-def gen_repeat(motif_len: int, repeat_n: int, mutation_rate: float, tab: dict):
-    # Generate a random motif with the given length
+def gen_repeat(motif_len: int, repeat_n: int, mutation_rate: float, flank_l: int, flank_r: int, tab: dict):
     alphabet = 'ACGT'
+    text = ''
+    for i in range(0, flank_l):
+        text += alphabet[random.randint(0, 3)]
+    # Generate a random motif with the given length
     pattern = list()
     for i in range(motif_len):
         pattern.append(alphabet[random.randint(0, 3)])
@@ -16,7 +19,6 @@ def gen_repeat(motif_len: int, repeat_n: int, mutation_rate: float, tab: dict):
         number_pool = -1
     else:
         number_pool = round(1 / mutation_rate)
-    text = ''
     for i in range(repeat_n):
         result = []
         for j in range(motif_len):
@@ -43,19 +45,23 @@ def gen_repeat(motif_len: int, repeat_n: int, mutation_rate: float, tab: dict):
         result = ''.join(result)
         # print('  Mutated motif: %s\n' % result)
         text += result
+    for i in range(0, flank_r):
+        text += alphabet[random.randint(0, 3)]
     tab['ID'].append(len(tab['ID']) + 1)
     tab['motif'].append(pattern)
     tab['periods'].append(repeat_n)
     tab['mutation'].append(mutated_n)
+    tab['flank_l'].append(flank_l)
+    tab['flank_r'].append(flank_r)
     tab['text'].append(text)
 
 
 if __name__ == '__main__':
-    table = {'ID': [], 'motif': [], 'periods': [], 'mutation': [], 'text': []}
-    gen_repeat(motif_len=3, repeat_n=5, mutation_rate=0.0, tab=table)
-    gen_repeat(motif_len=5, repeat_n=8, mutation_rate=0.02, tab=table)
-    gen_repeat(motif_len=10, repeat_n=20, mutation_rate=0.05, tab=table)
-    gen_repeat(motif_len=50, repeat_n=40, mutation_rate=0.07, tab=table)
-    gen_repeat(motif_len=100, repeat_n=100, mutation_rate=0.1, tab=table)
+    table = {'ID': [], 'motif': [], 'periods': [], 'mutation': [], 'flank_l': [], 'flank_r': [], 'text': []}
+    gen_repeat(motif_len=3, repeat_n=5, mutation_rate=0.0, flank_l=7, flank_r=8, tab=table)
+    gen_repeat(motif_len=5, repeat_n=8, mutation_rate=0.02, flank_l=20, flank_r=40, tab=table)
+    gen_repeat(motif_len=10, repeat_n=20, mutation_rate=0.05, flank_l=80, flank_r=80, tab=table)
+    gen_repeat(motif_len=50, repeat_n=40, mutation_rate=0.07, flank_l=150, flank_r=125, tab=table)
+    # gen_repeat(motif_len=100, repeat_n=100, mutation_rate=0.1, flank_l=2000, flank_r=3000, tab=table)
     df = pd.DataFrame(table)
     df.to_csv('motif.csv', index=False)
