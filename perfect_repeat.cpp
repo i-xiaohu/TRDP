7c8789ee464e1502743f9fcd0240c2e1d2b44ee5
i-xiaohu <631751599@qq.com>
2026-01-19 11:05:24 +0800

    perfect_repeat.cpp: calculate longest prefix-suffix match.

--- perfect_repeat.cpp
+++ perfect_repeat.cpp
@@ -7,50 +7,118 @@
 #include <cassert>
 using namespace std;
 
-void kmp_style(const string &s) {
-	// KMP
+// Longest prefix-substring match within S
+// Let K = LPS[i], S[0:K) = S[i:i+K)
+vector<int> calc_lps(const string &s) {
 	const int n = s.length();
-	vector<int> lps(n); // Longest prefix-suffix match length
+	vector<int> lps(n); // lps[i] = max{len | S[0:len)=S[i:i+len)}
 	lps[0] = 0;
-	for (int i = 1, j = 0; i < n; ) {
-		// s[0:j) = s[i-j: i)
-		if (s[i] == s[j]) { // Extension of a match
-			lps[i++] = ++j;
-		} else {
-			if (j == 0) { // Mismatch
-				lps[i++] = 0;
-			} else {
-				j = lps[j-1]; // Backward jump
+	int len = 0;
+	while (1+len < n and s[0+len] == s[1+len]) len++;
+	lps[1] = len;
+
+	int k = 1;
+	for (int i = 2; i < n; i++) {
+		int u = k + lps[k];
+		if (u > i) {
+			int t = u - i;
+			// S[i:u) equals to S[l-t:l), which serves as a bridge
+			for (int j = 0; j < t; j++) {
+				assert(s[i+j] == s[lps[k]-t+j]);
 			}
+			// Compare with prefix
+			int x = lps[k] - t;
+			if (lps[x] < t) {
+				lps[i] = lps[x];
+			} else {
+				// The match can further extend
+				len = t;
+				while (i + len < n and s[0+len] == s[i+len]) len++;
+				lps[i] = len;
+			}
+		} else {
+			len = 0;
+			while (i + len < n and s[0+len] == s[i+len]) len++;
+			lps[i] = len;
+		}
+		if (i + lps[i] > k + lps[k]) {
+			k = i;
+		}
+	}
+
+	// Check correctness
+	for (int i = 1; i < n; i++) {
+//		printf("i=%d, lps=%d\n", i, lps[i]);
+		len = lps[i];
+		for (int j = 0; j < len; j++) {
+			assert(s[0+j] == s[i+j]);
+		}
+		assert(i+len == n or s[len] != s[i+len]);
+	}
+	return lps;
+}
+
+// Longest substring-prefix match between T and Q
+// Let LSP[i] = K, T[i:i+K) = Q[0:K)
+void calc_lsp(const string &t, const string &q) {
+	const int n = t.length();
+	const int m = q.length();
+	vector<int> lsp(n);
+	for (int i = 0; i < n and i < m; i++) {
+		if (q[i] != t[i]) {
+			lsp[0] = i;
+			break;
+		}
+	}
+	vector<int> tmp = calc_lps(q);
+	int k = 0;
+	for (int i = 1; i < n; i++) {
+		int u = k + lsp[k];
+		if (u > i) {
+			// Similarly, T[i:i+t) = Q[l-t:l) is the bridge
+			int tt = u - i;
+			for (int j = 0; j < tt; j++) {
+				assert(t[i+j] == q[lsp[k]-tt+j]);
+			}
+			int x = lsp[k] - tt;
+			if (tmp[x] < tt) {
+				lsp[i] = tmp[x];
+			} else {
+				int len = tt;
+				while (i + len < n and len < m and q[0+len] == t[i+len]) len++;
+				lsp[i] = len;
+			}
+		} else {
+			int len = 0;
+			while (i + len < n and len < m and q[0+len] == t[i+len]) len++;
+			lsp[i] = len;
+		}
+		if (i + lsp[i] > k + lsp[k]) {
+			k = i;
 		}
 	}
 	// Check correctness
-	for (int i = 0; i < n; i++) {
-		int l = lps[i];
-		printf("i=%d, len=%d, s[%d:%d) = s[%d:%d]\n", i, l, 0, l, i-l+1, i);
-		// s[0:l) = s[i-l+1:i]
-		for (int j = 0; j < l; j++) {
-			assert(s[j] == s[i-l+1+j]);
+	for (int i = 1; i < n; i++) {
+//		printf("i=%d, lsp=%d\n", i, lsp[i]);
+		int len = lsp[i];
+		for (int j = 0; j < len; j++) {
+			assert(q[0+j] == t[i+j]);
 		}
-		// I didn't test if a longer match exists
+		assert(i+len == n or len == m or q[len] != t[i+len]);
 	}
-
-	vector<int> mid(n, 0); // mid(i) = max{l | s[0:l) = s[i,i+l)}
-	// Use _lps_ to infer _mid_
-	for (int i = 0; i < n; i++) {
-		int l = lps[i];
-		if (l > 0) {
-			mid[i-l+1] = max(mid[i-l+1], l);
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		printf("mid[%d]=%d\n", i, mid[i]);
-	}
-
 }
 
 int main(int argc, char *argv[]) {
-	string s = "ababac";
-	kmp_style(s);
+	srand(time(nullptr));
+	string s, q;
+	for (int i = 0; i < 1000; i++) {
+		s += "ACGT"[rand() % 4];
+	}
+	calc_lps(s);
+	calc_lps(q);
+	for (int i = 0; i < 200; i++) {
+		q += "ACGT"[rand() % 4];
+	}
+	calc_lsp(s, q);
 	return 0;
 }
