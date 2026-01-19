742b75e90e7c7144b4c13c581d3d38cecf5e1827
i-xiaohu <631751599@qq.com>
2026-01-19 16:27:09 +0800

    perfect_repeat.cpp: find repetitions centered at the right part.

--- perfect_repeat.cpp
+++ perfect_repeat.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <cassert>
+#include <algorithm>
 using namespace std;
 
 // Longest prefix-substring match within S
@@ -60,16 +61,14 @@ vector<int> calc_lps(const string &s) {
 
 // Longest substring-prefix match between T and Q
 // Let LSP[i] = K, T[i:i+K) = Q[0:K)
-void calc_lsp(const string &t, const string &q) {
+vector<int> calc_lsp(const string &t, const string &q) {
 	const int n = t.length();
 	const int m = q.length();
 	vector<int> lsp(n);
-	for (int i = 0; i < n and i < m; i++) {
-		if (q[i] != t[i]) {
-			lsp[0] = i;
-			break;
-		}
-	}
+	int len = 0;
+	while (len < n and len < m and q[len] == t[len]) len++;
+	lsp[0] = len;
+
 	vector<int> tmp = calc_lps(q);
 	int k = 0;
 	for (int i = 1; i < n; i++) {
@@ -84,12 +83,12 @@ void calc_lsp(const string &t, const string &q) {
 			if (tmp[x] < tt) {
 				lsp[i] = tmp[x];
 			} else {
-				int len = tt;
+				len = tt;
 				while (i + len < n and len < m and q[0+len] == t[i+len]) len++;
 				lsp[i] = len;
 			}
 		} else {
-			int len = 0;
+			len = 0;
 			while (i + len < n and len < m and q[0+len] == t[i+len]) len++;
 			lsp[i] = len;
 		}
@@ -98,27 +97,80 @@ void calc_lsp(const string &t, const string &q) {
 		}
 	}
 	// Check correctness
-	for (int i = 1; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 //		printf("i=%d, lsp=%d\n", i, lsp[i]);
-		int len = lsp[i];
+		len = lsp[i];
 		for (int j = 0; j < len; j++) {
 			assert(q[0+j] == t[i+j]);
 		}
 		assert(i+len == n or len == m or q[len] != t[i+len]);
 	}
+	return lsp;
+}
+
+// Search the repetitions formed by concatenating Q and T and centered at somewhere in T
+void find_center_at_right(string &q, string &t) {
+	const int n = t.length();
+	const int m = q.length();
+	// The prefix-substring match within T
+	vector<int> lps = calc_lps(t);
+	// The suffix-substring match between T and Q
+	reverse(t.begin(), t.end());
+	reverse(q.begin(), q.end());
+	vector<int> lss = calc_lsp(t, q);
+	reverse(t.begin(), t.end());
+	reverse(q.begin(), q.end());
+	reverse(lss.begin(), lss.end());
+	// Check correctness
+	for (int i = 0; i < n; i++) {
+		int len = lss[i];
+		for (int j = 0; j < len; j++) {
+			assert(q[m-1-j] == t[i-j]);
+		}
+	}
+	int max_len = min((n + m) / 2, n); // Maximum repeat unit length
+	for (int u = 1; u <= max_len; u++) { // Iterate over unit length
+		// The endpoints of repeats with a same length are consecutive
+		// If repeat ends at i in T, then
+		//     T[0:i-u] = T[u:i], i.e., LPS[u] >= i-u+1
+		//     Q[m-2u+i+1:m-1] = T[i-u+1:u-1], i.e., LSS[u-1] >= 2u-i-1
+		int end = min(2 * u - 2, u + (u < n ?lps[u] :0) - 1);
+		int beg = max(u - 1, 2 * u - lss[u-1] - 1);
+		if (beg <= end) {
+			fprintf(stderr, "Find %d repeats length of %d, ending at [%d,%d]\n", end-beg+1, u, beg, end);
+			for (int i = beg; i <= end; i++) {
+				for (int j = 0; j < i-u+1; j++) {
+					assert(t[j] == t[u+j]);
+				}
+				for (int j = 0; j < 2*u-i-1; j++) {
+					assert(q[m-2*u+i+1 + j] == t[i-u+1 + j]);
+				}
+			}
+		}
+	}
+}
+
+void divide_and_conquer(const string &s, int l, int r) {
+//	fprintf(stderr, "[%d,%d)\n", l, r);
+	if (r - l <= 1) {
+		return ;
+	}
+	int m = (l + r) / 2;
+	string a = s.substr(0, m);
+	string b = s.substr(m, r);
+	find_center_at_right(a, b);
+	divide_and_conquer(s, m, r);
+	// TODO: find center at left
+	divide_and_conquer(s, l, m);
 }
 
 int main(int argc, char *argv[]) {
 	srand(time(nullptr));
-	string s, q;
+	string s;
 	for (int i = 0; i < 1000; i++) {
 		s += "ACGT"[rand() % 4];
 	}
-	calc_lps(s);
-	calc_lps(q);
-	for (int i = 0; i < 200; i++) {
-		q += "ACGT"[rand() % 4];
-	}
-	calc_lsp(s, q);
+	divide_and_conquer(s, 0, s.length());
+	fprintf(stderr, "passed\n");
 	return 0;
 }
